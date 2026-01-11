b15d45102453d1e12c57a7468fda6c930529f86b
kammabhargav8008 <kammabhargav8008@gmail.com>
2026-01-11 08:49:49 +0530

    Create Footer.jsx

---
+++ Footer.jsx
@@ -0,0 +1,8 @@
+function Footer() {
+  return (
+    <footer className="bg-black text-white text-center p-4">
+      © {new Date().getFullYear()} Kamma Bhargav
+    </footer>
+  );
+}
+export default Footer;
